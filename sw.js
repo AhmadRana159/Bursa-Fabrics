e97eeb1fd7b96efb5294d4d8417adad0ba65a3ff
AhmadRana159 <fayyaz.ahmad.og@gmail.com>
2026-08-27 00:17:21 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,25 @@
+const CACHE_NAME = 'bursa-store-v1';
+const urlsToCache = [
+    './',
+    './index.html'
+];
+
+// 1. انسٹال کرتے وقت فائلیں کیش کرنا
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.use('bursa-store').then((cache) => {
-      return cache.addAll(['/']);
-    })
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache successfully');
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
+// 2. فچ (Fetch) کرتے وقت آف لائن یا آن لائن ڈیٹا سنبھالنا
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            // اگر نیٹ موجود ہے تو نیٹ سے لو، ورنہ کیش (آف لائن) سے دکھا دو
+            return response || fetch(e.request);
+        })
+    );
 });
